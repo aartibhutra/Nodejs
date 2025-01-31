0a1c9de7a446b17b40a616928e0f6b7fde8b6e33
aartibhutra <aartimaheshwari160@gmail.com>
2025-01-31 10:58:50 +0530

    add multiple exports

--- Math.js
+++ Math.js
@@ -2,23 +2,33 @@
 
 
 // this function is a private function foe these module if we want to use this function then we have to export(public this function) it.
-function add(a,b){
-    return a+b;
-}
+// function add(a,b){
+//     return a+b;
+// }
 
-function sub(a,b){
-    return a-b;
-}
+// function sub(a,b){
+//     return a-b;
+// }
+
+
 
 // module is an object use the method exports to export the function
 // module.exports = "Aarti";
 
 // it is equal to the add function
-module.exports = add;
-module.exports = sub;// this is wrong, because it override the value (add function)
+// module.exports = add;
+// module.exports = sub;// this is wrong, because it override the value (add function)
 
-// so avoid override function we use the javascript object to return multiple things 
-module.exports = {
-    add,// you also rename like addfn : add
-    sub,// here we rename sub function as sunfn : sun
-};
+// so avoid override function we use the javascript object to return multiple things so we call it as single export(default export)
+// module.exports = {
+//     add,// you also rename like addfn : add
+//     sub,// here we rename sub function as sunfn : sun
+// };
+
+
+// another way to export like use the export object 
+// multiple exports 
+
+exports.add = (a,b) => a+b;
+exports.sub = (a,b) => a-b;
+
--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
 // console.log("hello");
 
-// require use to import module(file use ./ means current directory) or package just like a import in the java 
+// require use to import module(file uses  ./ means current directory) or package just like a import in the java , require function use to give the path 
+// here math is a variable .
 
 const math = require('./Math');
 // in above we destructured the math like {add, sub} and direct use the add and sub like add(2,4)
+
 console.log("Math Value is",math);
 
 // pass two value to call the add function
