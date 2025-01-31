476e88c9470b50f0daa3d13ca1502659b0429439
aartibhutra <aartimaheshwari160@gmail.com>
2025-01-31 09:09:35 +0530

    add js objects

--- Math.js
+++ Math.js
@@ -1,9 +1,24 @@
+// here anything you make like function and objects you have to export them , then they available in other modules (files)
+
+
 // this function is a private function foe these module if we want to use this function then we have to export(public this function) it.
 function add(a,b){
     return a+b;
 }
+
+function sub(a,b){
+    return a-b;
+}
+
 // module is an object use the method exports to export the function
 // module.exports = "Aarti";
 
 // it is equal to the add function
 module.exports = add;
+module.exports = sub;// this is wrong, because it override the value (add function)
+
+// so avoid override function we use the javascript object to return multiple things 
+module.exports = {
+    add,// you also rename like addfn : add
+    sub,// here we rename sub function as sunfn : sun
+};
--- hello.js
+++ hello.js
@@ -2,8 +2,13 @@
 
 // require use to import module(file use ./ means current directory) or package just like a import in the java 
 
-const math = require('./Math')
+const math = require('./Math');
+// in above we destructured the math like {add, sub} and direct use the add and sub like add(2,4)
 console.log("Math Value is",math);
 
 // pass two value to call the add function
-console.log("Math Value is",math(4,5)); 
+// console.log("Math Value is",math(4,5)); 
+
+// use the javascript object function with the help of the .
+console.log(math.add(2,4));
+
