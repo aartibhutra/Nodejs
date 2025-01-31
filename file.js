57749bec484ccfc6ccbd5ce37ac898572d8e76c5
aartibhutra <aartimaheshwari160@gmail.com>
2025-01-31 16:01:28 +0530

    add changes

--- file.js
+++ file.js
@@ -15,10 +15,12 @@ const fs = require("fs");
 
 // read file :
 //Sync: when we use this then the sync give result in the form of variable 
+// sync return the result 
 // const result = fs.readFileSync('./contact.txt',"utf-8");
 // console.log(result);
 
 //Async: but here we use the callback function bcz Async doesn't give the value to the variable 
+// basically Async return nothing 
 fs.readFile('./contact.txt',"utf-8",(err,result)=>{
     if(err){
         console.log("Error");
