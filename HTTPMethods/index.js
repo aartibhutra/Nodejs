e0fa1b1045e9a32106b840de715710f3ba945a70
aartibhutra <aartimaheshwari160@gmail.com>
2025-02-03 10:23:15 +0530

    add changes

---
+++ HTTPMethods/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+// this create a web server for us 
+// you need handler function to handle the incoming request 
+//(req,res)=>{} this is responsible for process the incoming request
+const myServer = http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
+    // res is the response object that we can use to send data back to the client
+    // console.log("New Req Rec");
+    // console.log(req);// all the data that the client sent to the server is stored in the req object
+    // res.end("Hello From Server Again")
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myurl = url.parse(req.url,true); // here true pass the query string to the object
+    // const myurl = url.parse(req.url);
+    console.log(myurl);
+    
+    fs.appendFile("log.txt",log,(err,data)=>{
+        // res.end(" hello form server Again");
+        // Multi-route
+        // switch(req.url)
+        switch(myurl.pathname){
+            // / is denote to the home page 
+            case "/":
+                res.end("Home Page");
+                break;
+            case "/about":
+                const username = myurl.query.myname;
+                res.end(`Hello ${username} This is About Page`);
+                // res.end("I am Aarti Bhutra");
+                break;
+            case "/search":
+                const search = myurl.query.search_query;
+                res.end("here are your results for "+ search);
+                break;
+            default:
+                res.end("404 Not Found");
+
+        }
+    });
+    
+});
+myServer.listen(8001,() => console.log("Server started")
+);
