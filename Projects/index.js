1cd460d1f45279df95e98b77c0b7f4fc9e165c5c
aartibhutra <aartimaheshwari160@gmail.com>
2025-02-19 14:50:36 +0530

    GET

--- Projects/index.js
+++ Projects/index.js
@@ -4,5 +4,8 @@ const app = express();
 const PORT = 8000;
 
 // HERE we define our routes
+app.get('/users', (req, res) => {
+    return res.json(users);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
