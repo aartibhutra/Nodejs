d8bfa8e6c4989ae62adec052e108c8cafe25b2d4
aartibhutra <aartimaheshwari160@gmail.com>
2025-02-23 06:22:40 +0530

    header

--- Headers/index.js
+++ Headers/index.js
@@ -41,6 +41,7 @@ app.route("/api/users/:id").get((req,res) => {
     //get the id 
     const id =Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({error : 'user not found'});
     return res.json(user);
 }).patch((req,res) =>{
     // edit user with id 
